20aa522233d09841d534ca037c915e50c9a7586c
fchavonet <fchavonet@mailo.com>
2024-01-01 09:42:48 +0100

    First upload of task "0. Basic annotations - add".

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Module with a simple addition function. """
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add two floating-point numbers.
+
+    Args:
+        a (float): The first operand.
+        b (float): The second operand.
+
+    Returns:
+        float: The sum of the two input numbers.
+    """
+    return (a + b)
