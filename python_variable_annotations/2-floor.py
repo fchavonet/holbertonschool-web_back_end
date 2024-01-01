3708148a07577ebd0c3b822b2e5c147639d731f6
fchavonet <fchavonet@mailo.com>
2024-01-01 09:52:44 +0100

    First upload of task "2. Basic annotations - floor".

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Module with a floor conversion function. """
+
+
+def floor(n: float) -> int:
+    """
+    Convert a float to its floor value as an integer.
+
+    Args:
+        n (float): the floating-point number to convert.
+
+    Returns:
+        int: the floor value of the input float `n` as an integer.
+    """
+    return (int(n))
