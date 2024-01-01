b1f8906602432a85b3a56ee36ae0bf686bb71af8
fchavonet <fchavonet@mailo.com>
2024-01-01 10:14:37 +0100

    First upload of the task "7. Complex types - string and int/float to tuple".

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module with a type-annotated function to convert a string
+and an int OR float to a tuple.
+"""
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Convert a string and an int OR float to a tuple.
+
+    Args:
+        k (str): the input string.
+        v (Union[int, float]): the input integer or float.
+
+    Returns:
+        Tuple[str, float]: a tuple containing the string k
+                           and the square of v as a float.
+    """
+    return (k, v ** 2)
