1a5026eed0fafc8638fa5f8c86b672f822da8234
fchavonet <fchavonet@mailo.com>
2024-01-01 09:48:05 +0100

    First uplpoad of task "1. Basic annotations - concat".

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Module with a string concatenation function. """
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings.
+
+    Args:
+        str1 (str): the first string to concatenate.
+        str2 (str): the second string to concatenate.
+
+    Returns:
+        str: the concatenated string of `str1` and `str2`.
+    """
+    return (str1 + str2)
