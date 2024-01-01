fb6ac5147ec44e4caa5f6d98564095cd0ac86cd1
fchavonet <fchavonet@mailo.com>
2024-01-01 09:58:37 +0100

    First upload of task "4. Define variables".

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Module with annotated variables. """
+
+# An integer with a value of 1.
+a: int = 1
+
+# A float with a value of 3.14.
+pi: float = 3.14
+
+# A boolean with a value of True.
+i_understand_annotations: bool = True
+
+# A string with a value of “Holberton”.
+school: str = "Holberton"
