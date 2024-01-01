234a6f0831c8cafc2683d1ce0a1fcbbefdeddb60
fchavonet <fchavonet@mailo.com>
2024-01-01 10:26:56 +0100

    First upload of task "10. Duck typing - first element of a sequence".

---
+++ python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Module with a duck-typed function
+to safely retrieve the first element of a sequence.
+"""
+
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    Duck-typed function to safely retrieve the first element of a sequence.
+
+    Args:
+        lst (Sequence[Any]): a sequence of elements.
+
+    Returns:
+        Union[Any, None]: the first element of the sequence
+                          or None if the sequence is empty.
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
