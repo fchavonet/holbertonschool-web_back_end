324877d47855ca6e09f9af07a56560da1d16db8f
fchavonet <fchavonet@mailo.com>
2024-01-01 09:55:18 +0100

    First upload of task "3. Basic annotations - to string".

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Module with a string conversion function. """
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float to its string representation.
+
+    Args:
+        n (float): the floating-point number to convert.
+
+    Returns:
+        str: the string representation of the input float `n`.
+    """
+    return (str(n))
