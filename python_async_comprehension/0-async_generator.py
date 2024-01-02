f42db3cc6cebb4472dfa16394d83712bd6d1b9c1
fchavonet <fchavonet@mailo.com>
2024-01-02 11:17:40 +0100

    First upload of task "0. Async Generator".

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Async Generator Module. """
+
+import asyncio
+import random
+import typing
+
+
+async def async_generator() -> typing.AsyncGenerator[float, None]:
+    """
+    Generate random numbers asynchronously.
+
+    Yields:
+        float: random number between 0 and 10.
+
+    Raises:
+        None
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
