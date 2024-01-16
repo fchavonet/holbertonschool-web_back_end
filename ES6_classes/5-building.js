166d189fddf47c2ee1a6886caf7c7f457493b6af
fchavonet <fchavonet@mailo.com>
2024-01-16 10:42:13 +0100

    Fix: override the method named evacuationWarningMessage.

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
       throw Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
