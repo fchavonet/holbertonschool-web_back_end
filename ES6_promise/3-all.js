5e65818a616583b79e6d6d8ab4620416f67b3dc4
fchavonet <fchavonet@mailo.com>
2024-01-26 08:39:33 +0100

    Fix: forgot return.

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const promises = [uploadPhoto(), createUser()];
 
-  Promise.all(promises)
+  return Promise.all(promises)
     .then((results) => {
       console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
     })
