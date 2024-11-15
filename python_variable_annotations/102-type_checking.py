b3036d809b1a8d39a2bf3be6c93dd7f5c14af985
fchavonet <fchavonet@mailo.com>
2024-11-15 10:44:14 +0100

    Fix: type annotations to match checker requirements.

--- python_variable_annotations/102-type_checking.py
+++ python_variable_annotations/102-type_checking.py
@@ -1,22 +1,19 @@
-#!/usr/bin/env python3
-""" Module with a type-annotated function for zooming in on an array. """
-
 from typing import Tuple, List
 
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
     Zoom in on an array by repeating each element.
 
     Args:
-        lst (Tuple[int, ...]): the input tuple.
+        lst (Tuple): the input tuple.
         factor (int, optional): the zoom factor. Defaults to 2.
 
     Returns:
-        List[int]: the zoomed-in array with each element
-                   repeated according to the factor.
+        List: the zoomed-in array with each element
+              repeated according to the factor.
     """
-    zoomed_in: List[int] = [
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
