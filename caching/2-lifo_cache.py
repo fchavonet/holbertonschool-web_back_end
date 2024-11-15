0f258f44353e7f893ea4ccceb0d5fb9004784d58
fchavonet <fchavonet@mailo.com>
2024-11-15 15:58:30 +0100

    Creating "LIFO Cache" module.

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+"""
+LIFO Cache module.
+"""
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    LIFOCache class.
+    Implements a LIFO caching system.
+    """
+
+    def __init__(self):
+        """
+        Initialize the cache.
+        """
+        super().__init__()
+        self.last_key = None
+
+    def put(self, key, item):
+        """
+        Add an item to the cache using LIFO policy.
+        If key or item is None, do nothing.
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+            self.last_key = key
+
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                if self.last_key in self.cache_data:
+                    del self.cache_data[self.last_key]
+                    print(f"DISCARD: {self.last_key}")
+
+                self.last_key = key
+
+    def get(self, key):
+        """
+        Get an item by key.
+        If key is None or the key is not in the cache, return None.
+        """
+        return self.cache_data.get(key, None)
