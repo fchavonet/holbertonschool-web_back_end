48999bdeaa18611ff68226f9313b3d4b1699a3ea
fchavonet <fchavonet@mailo.com>
2025-02-01 13:52:57 +0100

    Creating a function to validate that the provided password matches the hashed password.

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -25,3 +25,21 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(encoded_password, salt)
 
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validate if the provided password matches the hashed password.
+
+    Args:
+        hashed_password (bytes): the hashed password to compare.
+        password (str): the plain text password to validate.
+
+    Returns:
+        bool: True if the password is valid, False otherwise.
+    """
+    # Encode the plain text password to bytes.
+    encoded_password = password.encode("utf-8")
+
+    # Use bcrypt to compare the password with the hash.
+    return bcrypt.checkpw(encoded_password, hashed_password)
