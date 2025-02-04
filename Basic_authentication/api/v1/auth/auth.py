367d2cf72e082f382cc7c3f675d13f3a335d995a
fchavonet <fchavonet@mailo.com>
2025-02-04 18:46:11 +0100

    Creating a class template to manage the API authentication.

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Auth module for the API.
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+         Determines if authentication is required for a given path.
+        For now, it always returns False.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Returns the value of the Authorization header, if present.
+        For now, it returns None.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns the current user.
+        For now, it returns None.
+        """
+        return None
