48b37acea3aaa66bf7310c59e2a9bbd911dad351
fchavonet <fchavonet@mailo.com>
2025-02-04 17:43:46 +0100

    Fix: pycodestyle on the index.py file.

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""" 
+"""
 Module of Index views.
 """
 
-from flask import jsonify, abort
 from api.v1.views import app_views
+from flask import jsonify, abort
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
