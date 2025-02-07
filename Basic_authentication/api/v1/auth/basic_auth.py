edb78a200de9890a1a36ed2bbcae938afe0884cd
fchavonet <fchavonet@mailo.com>
2025-02-07 14:34:17 +0100

    Fix: indentation problem!

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -64,28 +64,28 @@ class BasicAuth(Auth):
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
 
-        def extract_user_credentials(
-            self, decoded_base64_authorization_header: str
-        ) -> (str, str):
-            """
-            Extract the user email and password
-            from a decoded Base64 authorization header.
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str
+    ) -> (str, str):
+        """
+        Extract the user email and password
+        from a decoded Base64 authorization header.
 
-            Args:
-                decoded_base64_authorization_header (str):
-                    the decoded Base64 string.
+        Args:
+            decoded_base64_authorization_header (str):
+                the decoded Base64 string.
 
-            Returns:
-                tuple: A tuple (email, password) or (None, None) if invalid.
-            """
-            if decoded_base64_authorization_header is None:
-                return None, None
+        Returns:
+            tuple: A tuple (email, password) or (None, None) if invalid.
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
 
-            if not isinstance(decoded_base64_authorization_header, str):
-                return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
 
-            if ':' not in decoded_base64_authorization_header:
-                return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
 
-            email, password = decoded_base64_authorization_header.split(':', 1)
-            return email, password
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
