5afc9763b1bafa84957a6fb3b1f30540e1e82212
fchavonet <fchavonet@mailo.com>
2025-02-14 08:55:58 +0100

    Adding a new method in the class "BasicAuth" that returns the User instance based on his email and password.

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,8 @@ BasicAuth module for the API.
 
 import base64
 from api.v1.auth.auth import Auth
+from models.user import User
+from typing import TypeVar, Tuple
 
 
 class BasicAuth(Auth):
@@ -66,14 +68,14 @@ class BasicAuth(Auth):
 
     def extract_user_credentials(
         self, decoded_base64_authorization_header: str
-    ) -> (str, str):
+    ) -> Tuple[str, str]:
         """
         Extract the user email and password
         from a decoded Base64 authorization header.
 
         Args:
             decoded_base64_authorization_header (str):
-                the decoded Base64 string.
+            the decoded Base64 string.
 
         Returns:
             tuple: A tuple (email, password) or (None, None) if invalid.
@@ -89,3 +91,36 @@ class BasicAuth(Auth):
 
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
+
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+    ) -> TypeVar('User'):
+        """
+        Retrieves a User instance based on email and password.
+
+        Args:
+            user_email (str): the user's email address.
+            user_pwd (str): the user's password.
+
+        Returns:
+            User | None: the User instance if found and valid, otherwise None.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+
+        if not users or len(users) == 0:
+            return None
+
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
