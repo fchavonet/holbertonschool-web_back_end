c9b9b8e1ee9f45606b4f51301dd481f3b5168938
fchavonet <fchavonet@mailo.com>
2025-03-06 14:26:28 +0100

    Creating a new class swith the "register_user" method.

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,10 +1,55 @@
 #!/usr/bin/env python3
 
 """
-Module providing password hashing functionality using bcrypt.
+Module providing user authentication functionality.
 """
 
-import bcrypt
+from db import DB
+from user import User
+from bcrypt import hashpw, gensalt
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """
+    Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def _hash_password(self, password: str) -> bytes:
+        """
+        Hashes a password using bcrypt.
+
+        Args:
+            password (str): the password to hash.
+
+        Returns:
+            bytes: the hashed password.
+        """
+        return hashpw(password.encode("utf-8"), gensalt())
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Registers a new user if the email is not already taken.
+
+        Args:
+            email (str): the user's email.
+            password (str): the user's password.
+
+        Returns:
+            User: the newly created user object.
+
+        Raises:
+            ValueError: if a user with the given email already exists.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = self._hash_password(password)
+            return self._db.add_user(email, hashed_password)
 
 
 def _hash_password(password: str) -> bytes:
