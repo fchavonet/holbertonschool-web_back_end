4c5ff59b23ca21a21396d9f124a60095250498bb
fchavonet <fchavonet@mailo.com>
2025-03-14 09:17:36 +0100

    Fix: I'm stupid today, define the last methot outside the "Auth" class...

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -130,6 +130,27 @@ class Auth:
 
         self._db.update_user(user_id, session_id=None)
 
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Generates a reset password token for a user.
+
+        Args:
+            email (str): the user's email.
+
+        Returns:
+            str: the generated reset password token.
+        """
+
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+
+        return reset_token
+
 
 def _hash_password(password: str) -> bytes:
     """
@@ -154,25 +175,3 @@ def _generate_uuid() -> str:
     """
 
     return str(uuid.uuid4())
-
-
-def get_reset_password_token(self, email: str) -> str:
-    """
-    Generates a reset password token for a user.
-
-    Args:
-        email (str): the user's email.
-
-    Returns:
-        str: the generated reset password token.
-    """
-
-    try:
-        user = self._db.find_user_by(email=email)
-    except NoResultFound:
-        raise ValueError
-
-    reset_token = _generate_uuid()
-    self._db.update_user(user.id, reset_token=reset_token)
-
-    return reset_token
