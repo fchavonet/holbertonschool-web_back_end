4eb4dbff84156657f35901909726e7a52e7d5712
fchavonet <fchavonet@mailo.com>
2025-03-23 19:17:08 +0100

    Creating test for "GithubOrgClient._public_repos_url" using property patching.

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """
 Unit tests for the client module.
 """
@@ -7,7 +6,7 @@ Unit tests for the client module.
 import unittest
 from client import GithubOrgClient
 from parameterized import parameterized
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -25,6 +24,7 @@ class TestGithubOrgClient(unittest.TestCase):
         Test that "GithubOrgClient.org" returns the correct value.
         Test that "get_json" is called exactly once with the expected URL.
         """
+
         fake_payload = {"login": org, "id": 123}
         mock_get_json.return_value = fake_payload
 
@@ -33,6 +33,26 @@ class TestGithubOrgClient(unittest.TestCase):
         expected_url = f"https://api.github.com/orgs/{org}"
         mock_get_json.assert_called_once_with(expected_url)
         self.assertEqual(result, fake_payload)
+
+    def test_public_repos_url(self) -> None:
+        """
+        Test that GithubOrgClient._public_repos_url returns the correct URL
+        based on the mocked org property.
+        """
+
+        fake_payload = {
+            "repos_url": "https://api.github.com/orgs/google/repos"
+        }
+        with patch(
+            "client.GithubOrgClient.org",
+            new_callable=PropertyMock
+        ) as mock_org:
+            mock_org.return_value = fake_payload
+            client_instance = GithubOrgClient("google")
+            self.assertEqual(
+                client_instance._public_repos_url,
+                fake_payload["repos_url"]
+            )
 
 
 if __name__ == "__main__":
