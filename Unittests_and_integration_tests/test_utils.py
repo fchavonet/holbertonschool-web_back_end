8b27012235e7864f22b9402591d7c60964867a1a
fchavonet <fchavonet@mailo.com>
2025-03-23 18:43:18 +0100

    Creating test for the "memoize" decorator.

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -8,7 +8,7 @@ import unittest
 from parameterized import parameterized
 from typing import Any, Dict, Tuple
 from unittest.mock import patch
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -76,6 +76,46 @@ class TestGetJson(unittest.TestCase):
             result = get_json(test_url)
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    Test class for the "memoize" decorator.
+    """
+
+    def test_memoize(self) -> None:
+        """
+        Test that memoize caches the result of a_method.
+        """
+
+        class TestClass:
+            """
+            Test class to validate the memoize decorator.
+            """
+
+            def a_method(self) -> int:
+                """
+                Returns an integer value.
+                """
+                return 42
+
+            @memoize
+            def a_property(self) -> int:
+                """
+                Returns the result of a_method.
+                """
+
+                return self.a_method()
+
+        with patch.object(
+            TestClass, "a_method", return_value=42
+        ) as mocked_method:
+            test_instance = TestClass()
+            first_call = test_instance.a_property
+            second_call = test_instance.a_property
+            self.assertEqual(first_call, 42)
+            self.assertEqual(second_call, 42)
+            mocked_method.assert_called_once()
 
 
 if __name__ == "__main__":
