332f58ce6f878e1607dfdbad27744087e7b8da44
fchavonet <fchavonet@mailo.com>
2025-03-24 12:04:49 +0100

    Creating a basic Flask app with a HTML template.

---
+++ i18n/0-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+Basic Flask application with a single route.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """
+    Renders the index page with a "Hello world"" message.
+
+    Returns:
+        Rendered HTML template for the "root" route.
+    """
+
+    return render_template("0-index.html")
+
+
+if __name__ == '__main__':
+    app.run()
