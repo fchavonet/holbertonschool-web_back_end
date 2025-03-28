63f1ccb05c742ff2846b049532099713e4d41706
fchavonet <fchavonet@mailo.com>
2025-03-28 09:49:57 +0100

    Creating a "Cache" class that interacts with "Redis".

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+"""
+Defines a Cache class that interacts with Redis.
+"""
+
+import redis
+import uuid
+
+class Cache():
+    """
+    Cache class for storing data in Redis.
+    """
+
+    def __init__(self):
+        """
+        Initialize the Cache instance.
+        Connects to Redis and clears the current database.
+        """
+
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data):
+        """
+        Store the given data in Redis using a randomly generated key.
+
+        Args:
+            data: the data to store.
+
+        Returns:
+            str: the key under which the data was stored.
+        """
+
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
