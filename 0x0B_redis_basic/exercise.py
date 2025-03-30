cf586758cef4465fb50a5c2ad6f63a1562cabc90
fchavonet <fchavonet@mailo.com>
2025-03-30 13:51:59 +0200

    Adding "call_history" decorator to store method.

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -27,6 +27,26 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """
+    Decorator to store the history of inputs and outputs.
+    """
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Store input args and output result in Redis lists."""
+        input_key = method.__qualname__ + ":inputs"
+        output_key = method.__qualname__ + ":outputs"
+
+        self._redis.rpush(input_key, str(args))
+
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(result))
+
+        return result
+
+    return wrapper
+
+
 class Cache():
     """
     Cache class for storing data in Redis.
@@ -42,6 +62,7 @@ class Cache():
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store the given data in Redis using a randomly generated key.
