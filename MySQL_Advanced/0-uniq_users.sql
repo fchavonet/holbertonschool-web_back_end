572d969d4fb2ee71000b35249e9ffbd5039cf50b
fchavonet <fchavonet@mailo.com>
2025-04-08 10:11:06 +0200

    Creating users table with unique email.

---
+++ MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Create the users table with a unique email.
+
+CREATE TABLE IF NOT EXISTS users (
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        name VARCHAR(255)
+    );
