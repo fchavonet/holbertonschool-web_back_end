f953eae27cf4942def6d9a6baa183c0e1d4d1314
fchavonet <fchavonet@mail.fr>
2025-04-11 09:50:55 +0200

    Adding "SafeDiv" function for safe division.

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,7 @@
+-- Returns a divided by b, or 0 if b equals 0.
+DELIMITER / /
+DROP FUNCTION IF EXISTS SafeDiv;
+
+CREATE FUNCTION SafeDiv (a INT, b INT) RETURNS FLOAT DETERMINISTIC BEGIN RETURN (IF (b = 0, 0, a / b));
+
+END / / DELIMITER;
