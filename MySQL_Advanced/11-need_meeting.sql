33ac14b3ed4c4c03c6a9c77dd76a97d0a5f5af42
fchavonet <fchavonet@mail.fr>
2025-04-11 11:23:55 +0200

    Adding "need_meeting" view for students with low scores and outdated meetings.

---
+++ MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,6 @@
+-- List students with score < 80 and with no meeting or with a meeting older than 1 month.
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
