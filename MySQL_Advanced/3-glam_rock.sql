6cce6a42645e2a7412e9f907ef7a3eeae4808989
fchavonet <fchavonet@mail.fr>
2025-04-11 08:59:37 +0200

    Another test to pass the checker, don't find the problem with the code (works perfectly in local).

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,4 @@
--- Select glam rock bands and compute their lifespan.
-SELECT
-    name AS band_name,
-    COALESCE(split, 2020) - formed AS lifespan
-FROM
-    metal_bands
-WHERE
-    style = 'Glam rock'
-ORDER BY
-    lifespan DESC;
+-- create table users
+
+SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM metal_bands
+WHERE style LIKE '%Glam rock%' ;
