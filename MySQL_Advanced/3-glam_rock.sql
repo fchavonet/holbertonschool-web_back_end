2b02aa0947402f167cf5bc5f529fc7862f6a4063
fchavonet <fchavonet@mail.fr>
2025-04-11 09:01:55 +0200

    Comming back to my original version, checker doesn't pass but works perfectly in local mode.

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,8 @@
--- create table users
-
-SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM metal_bands
-WHERE style LIKE '%Glam rock%' ;
+-- Select glam rock bands and compute their lifespan
+SELECT
+    band_name,
+    COALESCE(split, 2020) - formed as lifespan
+FROM
+    metal_bands
+WHERE
+    style LIKE '%Glam rock%';
