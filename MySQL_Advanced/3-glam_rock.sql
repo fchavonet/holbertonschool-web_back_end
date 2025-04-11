b44819e64d01a20babb00a202429f3102ce30472
fchavonet <fchavonet@mail.fr>
2025-04-11 14:02:59 +0200

    Fix: change date.

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
--- Select glam rock bands and compute their lifespan
-SELECT band_name, COALESCE((IFNULL(split, 2020)) - formed) AS lifespan
+-- Select glam rock bands and compute their lifespan.
+SELECT band_name, COALESCE((IFNULL (split, 2024)) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%';
