e7bd0a09ca73d3a24302934007d230226571acaf
fchavonet <fchavonet@mail.fr>
2025-04-11 09:59:12 +0200

    Fix: help with ChatGPT to find solution, sybtax error, change all code, but understand why.

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,5 +1,13 @@
--- Returns a divided by b, or 0 if b equals 0
-DELIMITER / /
-DROP FUNCTION IF EXISTS SafeDiv / / CREATE FUNCTION SafeDiv (a INT, b INT) RETURNS FLOAT DETERMINISTIC BEGIN RETURN IF (b = 0, 0, a / b);
+-- Returns a divided by b, or 0 if b equals 0.
+DELIMITER $$
 
-END / / DELIMITER;
+DROP FUNCTION IF EXISTS SafeDiv$$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    RETURN IF(b = 0, 0, a / b);
+END$$
+
+DELIMITER ;
