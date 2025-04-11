fca252a4b178f87e6e0f5dce93f66d1d2fb02060
fchavonet <fchavonet@mail.fr>
2025-04-11 09:24:56 +0200

    Adding "AddBonus" procedure for bonus corrections.

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,13 +1,12 @@
 -- Reset valid_email to 0 if the email is updated.
 DELIMITER $$
+CREATE TRIGGER new_email BEFORE
+UPDATE ON users FOR EACH ROW BEGIN IF OLD.email <> NEW.email THEN
+SET
+    NEW.valid_email = 0;
 
-CREATE TRIGGER new_email 
-BEFORE UPDATE ON users
-FOR EACH ROW
-BEGIN
-    IF OLD.email <> NEW.email THEN
-        SET NEW.valid_email = 0;
-    END IF;
+END IF;
+
 END$$
 
 DELIMITER ;
--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -1,18 +1,44 @@
 -- Adds a bonus correction, creating the project if needed.
 DELIMITER $$
-
-CREATE PROCEDURE AddBonus(IN user_id INTEGER, IN project_name VARCHAR(255), IN score INTEGER)
-BEGIN
-    -- Insert the project if it does not already exist.
-    INSERT INTO projects(name)
-    SELECT project_name FROM DUAL
-    WHERE NOT EXISTS (
-        SELECT 1 FROM projects WHERE name = project_name
+CREATE PROCEDURE AddBonus (
+    IN user_id INTEGER,
+    IN project_name VARCHAR(255),
+    IN score INTEGER
+) BEGIN
+-- Insert the project if it does not already exist.
+INSERT INTO
+    projects (name)
+SELECT
+    project_name
+FROM
+    DUAL
+WHERE
+    NOT EXISTS (
+        SELECT
+            1
+        FROM
+            projects
+        WHERE
+            name = project_name
     );
 
-    -- Insert the correction with the associated user_id, project_id (looked up by project_name), and score.
-    INSERT INTO corrections (user_id, project_id, score)
-    VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
-END $$
+-- Insert the correction with the associated user_id, project_id (looked up by project_name), and score.
+INSERT INTO
+    corrections (user_id, project_id, score)
+VALUES
+    (
+        user_id,
+        (
+            SELECT
+                id
+            FROM
+                projects
+            WHERE
+                name = project_name
+        ),
+        score
+    );
+
+END$$
 
 DELIMITER ;
