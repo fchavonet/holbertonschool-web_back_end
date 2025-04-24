5c3a1625206657a17e8286b1f4cfee3ea9f394c7
fchavonet <fchavonet@mail.fr>
2025-04-24 20:42:34 +0200

    Creating tests file for "calculateNumber" function.

---
+++ unittests_in_js/0-calcul.test.js
@@ -0,0 +1,20 @@
+const assert = require("assert");
+const calculateNumber = require("./0-calcul");
+
+describe("calculateNumber", function () {
+  it("Sum when both are integers.", () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it("Sum when b is a float.", () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it("Sum when a is rounded down & b is rounded up.", () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it("Sum when both are rounded up.", () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+});
