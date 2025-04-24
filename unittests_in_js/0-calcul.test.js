13f0784cb05028884d0ca760ad32942efead4c95
fchavonet <fchavonet@mail.fr>
2025-04-24 20:48:29 +0200

    Fix: adding more tests to try to pass the checker.

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -6,8 +6,16 @@ describe("calculateNumber", function () {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
+  it("Sum when a is a float.", () => {
+    assert.strictEqual(calculateNumber(1.2, 3), 4);
+  });
+
   it("Sum when b is a float.", () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it("Sum when a is rounded up & b is rounded down.", () => {
+    assert.strictEqual(calculateNumber(1.7, 3.2), 5);
   });
 
   it("Sum when a is rounded down & b is rounded up.", () => {
@@ -15,6 +23,10 @@ describe("calculateNumber", function () {
   });
 
   it("Sum when both are rounded up.", () => {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(1.2, 3.2), 4);
+  });
+
+  it("Sum when both are rounded down.", () => {
+    assert.strictEqual(calculateNumber(1.7, 3.7), 6);
   });
 });
