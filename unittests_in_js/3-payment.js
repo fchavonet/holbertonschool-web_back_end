88dafd2dc27e9090f15514297d43bba722567809
fchavonet <fchavonet@mail.fr>
2025-04-25 09:37:33 +0200

    Fix: addig blank space after ":" in "The total is: "...

--- unittests_in_js/3-payment.js
+++ unittests_in_js/3-payment.js
@@ -2,7 +2,7 @@ const Utils = require("./utils.js");
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
 	const sum = Utils.calculateNumber("SUM", totalAmount, totalShipping);
-	console.log("The total is:" + sum);
+	console.log("The total is: " + sum);
 }
 
 module.exports = sendPaymentRequestToApi;
