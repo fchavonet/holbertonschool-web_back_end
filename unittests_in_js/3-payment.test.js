c152581821553a918355442050cf7f844cb61753
fchavonet <fchavonet@mail.fr>
2025-04-25 09:31:10 +0200

    Fix: didn't read the instructions properly, change of values to be tested.

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -7,13 +7,12 @@ const sendPaymentRequestToApi = require("./3-payment.js");
 describe("sendPaymentRequestToApi", function () {
     const calculateNumberSpy = sinon.spy(Utils, "calculateNumber");
 
-    it("validate the usage of the Utils function", () => {
-        sendPaymentRequestToApi(40, 2);
+    it("Validate the usage of the Utils function.", () => {
+        sendPaymentRequestToApi(100, 20);
 
         chai.expect(calculateNumberSpy.calledOnce).to.be.true;
-        chai.expect(calculateNumberSpy.calledWith("SUM", 40, 2)).to.be.true;
+        chai.expect(calculateNumberSpy.calledWith("SUM", 100, 20)).to.be.true;
 
         calculateNumberSpy.restore()
     });
 });
- 
