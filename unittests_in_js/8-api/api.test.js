ece885118ef3a0ee2ccee9078e8cf2bdec19d556
fchavonet <fchavonet@mail.fr>
2025-05-08 15:12:28 +0200

    Adding numeric-only /cart/:id endpoint and integration tests.

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -4,12 +4,12 @@ const request = require("request");
 describe("indexPageTest", function () {
 	it("Should return status code 200", function (done) {
 		request.get("http://localhost:7865", (error, response, body) => {
-			chai.expect(response.statusCode).to.equal(200)
-			chai.expect(body).to.equal("Welcome to the payment system")
+			chai.expect(response.statusCode).to.equal(200);
+			chai.expect(body).to.equal("Welcome to the payment system");
 
-			if (error) return done(error)
+			if (error) return done(error);
 
-			done()
-		})
-	})
-})
+			done();
+		});
+	});
+});
--- unittests_in_js/9-api/api.js
+++ unittests_in_js/9-api/api.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
 	res.send("Welcome to the payment system");
 });
 
+app.get("/cart/:id(\\d+)", (req, res) => {
+	res.status(200).send("Payment methods for cart " + req.params.id);
+});
+
 app.listen(port, () => {
 	console.log("API available on localhost port " + port);
 });
