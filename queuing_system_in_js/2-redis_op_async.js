0846739c4985d68888d581e0092e7dd9067e2024
fchavonet <fchavonet@mail.fr>
2025-05-19 11:28:19 +0200

    Implement "async Redis GET" operation using promisify and "async/await".

--- queuing_system_in_js/2-redis_op_async.js
+++ queuing_system_in_js/2-redis_op_async.js
@@ -1,4 +1,5 @@
 import { createClient, print } from "redis";
+import { promisify } from "util";
 
 const client = createClient();
 
@@ -14,14 +15,11 @@ function setNewSchool(schoolName, value) {
 	client.set(schoolName, value, print);
 }
 
-function displaySchoolValue(schoolName) {
-	client.get(schoolName, (error, value) => {
-		if (error) {
-			console.error("Error:", error);
-			return;
-		}
-		console.log(value);
-	});
+const getAsync = promisify(client.get).bind(client);
+
+async function displaySchoolValue(schoolName) {
+	const value = await getAsync(schoolName);
+	console.log(value);
 }
 
 displaySchoolValue("Holberton");
